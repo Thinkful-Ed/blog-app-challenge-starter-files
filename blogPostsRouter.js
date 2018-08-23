eb134d48bb2d96bdc448b597a59355fa77eb2c18
Alex Jacobs <lex.jacobs@gmail.com>
2018-08-22 19:48:55 -0700

    Update blogPostsRouter.js
    Fixes syntax error

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -72,7 +72,7 @@ router.put("/:id", (req, res) => {
   if (req.params.id !== req.body.id) {
     const message = `Request path id (${
       req.params.id
-    }) and request body id ``(${req.body.id}) must match`;
+    }) and request body id (${req.body.id}) must match`;
     console.error(message);
     return res.status(400).send(message);
   }
